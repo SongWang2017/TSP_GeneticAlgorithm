51012a68d332906c8b901380ff3d94cf3a93290a
Song Wang <wang.song@columbia.edu>
2018-08-01 12:26:16 -0700

    adding files

---
+++ src/TravelManShortestPath/TSP_GA.java
@@ -0,0 +1,93 @@
+package TravelManShortestPath;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class TSP_GA {
+	
+	
+
+    public static void main(String[] args) {
+    	
+    	String file = "/path/to/your/file.txt";
+
+    	try {
+
+    	    BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+    	    String line;
+    	    // Uncomment the line below if you want to skip the fist line (e.g if headers)
+    	    // line = br.readLine();
+
+    	    while ((line = br.readLine()) != null) {
+
+    	        // do something with line
+
+    	    }
+    	    br.close();
+
+    	} catch (IOException e) {
+    	    System.out.println("ERROR: unable to read file " + file);
+    	    e.printStackTrace();   
+    	}
+    	
+    	
+
+        // Create and add our cities
+        City city = new City(60, 200);
+        TourManager.addCity(city);
+        City city2 = new City(180, 200);
+        TourManager.addCity(city2);
+        City city3 = new City(80, 180);
+        TourManager.addCity(city3);
+        City city4 = new City(140, 180);
+        TourManager.addCity(city4);
+        City city5 = new City(20, 160);
+        TourManager.addCity(city5);
+        City city6 = new City(100, 160);
+        TourManager.addCity(city6);
+        City city7 = new City(200, 160);
+        TourManager.addCity(city7);
+        City city8 = new City(140, 140);
+        TourManager.addCity(city8);
+        City city9 = new City(40, 120);
+        TourManager.addCity(city9);
+        City city10 = new City(100, 120);
+        TourManager.addCity(city10);
+        City city11 = new City(180, 100);
+        TourManager.addCity(city11);
+        City city12 = new City(60, 80);
+        TourManager.addCity(city12);
+        City city13 = new City(120, 80);
+        TourManager.addCity(city13);
+        City city14 = new City(180, 60);
+        TourManager.addCity(city14);
+        City city15 = new City(20, 40);
+        TourManager.addCity(city15);
+        City city16 = new City(100, 40);
+        TourManager.addCity(city16);
+        City city17 = new City(200, 40);
+        TourManager.addCity(city17);
+        City city18 = new City(20, 20);
+        TourManager.addCity(city18);
+        City city19 = new City(60, 20);
+        TourManager.addCity(city19);
+        City city20 = new City(160, 20);
+        TourManager.addCity(city20);
+
+        // Initialize population
+        Population pop = new Population(50, true);
+        System.out.println("Initial distance: " + pop.getFittest().getDistance());
+
+        // Evolve population for 100 generations
+        pop = GA.evolvePopulation(pop);
+        for (int i = 0; i < 10000; i++) {
+            pop = GA.evolvePopulation(pop);
+        }
+
+        // Print final results
+        System.out.println("Finished");
+        System.out.println("Final distance: " + pop.getFittest().getDistance());
+        System.out.println("Solution:");
+        System.out.println(pop.getFittest());
+    }
+}
---
+++ src/TravelManShortestPath/readData.java
@@ -0,0 +1,5 @@
+package TravelManShortestPath;
+
+public class readData {
+
+}
